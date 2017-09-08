05d882873010466697e0b63d718d2d03b4050fa0
spanthetree <dwoodruff@opentable.com>
2017-09-08 12:34:55 -0700

    added id search for moviebot

--- moviebot/test.py
+++ moviebot/test.py
@@ -11,10 +11,15 @@ def searchmovie(ia, title):
     for name in ia.search_movie(title):
         print name.movieID, name['title']
 
+def getmovie(ia, movieid):
+    moviedata = ia.get_movie(movieid)
+    print moviedata.summary()
+
 def main():
     parser = argparse.ArgumentParser(description="test args")
     parser.add_argument('-t', '--title', help="movie title" )
     parser.add_argument('-p', '--person', help="person")
+    parser.add_argument('-i', '--movieid', help="id")
 
     args = parser.parse_args()
     ia = IMDb()
@@ -26,6 +31,10 @@ def main():
     if args.title:
         args = args.title.lower()
         searchmovie(ia, args)
+
+    if args.movieid:
+        args = args.movieid.lower()
+        getmovie(ia, args)
 
 
 if __name__ == "__main__":
